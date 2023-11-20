b7890bb6cff270e26d8ade6006188ef229a4277c
jonahgrigoryan1 <149550031+jonahgrigoryan1@users.noreply.github.com>
2023-11-19 20:12:33 -0800

    Update imputed.py

--- imputed.py
+++ imputed.py
@@ -44,3 +44,31 @@ from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler(feature_range=(0, 1))                                                                    
 train_data_scaled = scaler.fit_transform(train_data_inliers)                                                    
 train_data_scaled_df = pd.DataFrame(train_data_scaled, columns=train_data_inliers.columns)
+
+# Assuming train_data_scaled_df is the preprocessed dataset with 26 features                                    
+# Load the original dataset to get the 'stock_id' and 'seconds_in_bucket' columns                               
+original_data = pd.read_csv('subset_train_data.csv')                                      
+                                                                                                               
+# Merge the original dataset with the preprocessed one to get the 'stock_id' and 'seconds_in_bucket' for sequencing                                                                                                      
+merged_data = original_data[['stock_id', 'seconds_in_bucket']].merge(train_data_scaled_df, left_index=True,     
+right_index=True)                                                                                               
+                                                                                                               
+# Now perform the checks on merged_data                                                                         
+# Check for missing time steps within each stock_id                                                             
+for stock_id, group in merged_data.groupby('stock_id'):                                                         
+   # Assuming seconds_in_bucket starts at 0 and increments every 10 seconds                                    
+   expected_seconds = list(range(0, group['seconds_in_bucket'].max() + 10, 10))                                
+   if not all(second in group['seconds_in_bucket'].values for second in expected_seconds):                     
+       print(f"Missing data in stock_id {stock_id}")                                                           
+                                                                                                               
+# Verify data integrity                                                                                         
+if not merged_data.sort_values(by=['stock_id', 'seconds_in_bucket']).equals(merged_data):                       
+   print("Data is not sorted correctly or contains duplicates.")                                               
+                                                                                                               
+# Determine sequence length based on the frequency of seconds_in_bucket                                         
+# For example, if you want sequences representing the last 10 minutes and data is recorded every 10 seconds:    
+sequence_length = 9 * 60 // 9  # 54 data points for the last 10 minutes                                       
+                                                                                                               
+# Check if the sequence length is appropriate given the data frequency                                          
+if sequence_length > merged_data['seconds_in_bucket'].nunique():                                                
+   print("Sequence length is too long for the available time steps.") 
